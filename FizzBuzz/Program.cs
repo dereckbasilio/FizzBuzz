1e70d59fb37336e26dda347c89e4f1fa57f8ef6d
Dereck Basilio <dereck@docufy.com>
2013-07-02 15:49:02 -0500

    Moved FizzBuzz to Class Library

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using FizzBuzzClassLibrary;
 
 namespace FizzBuzz
 {
@@ -9,25 +10,8 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            for (int i = 1; i <= 100; i++)
-            {
-                if (i % 15 == 0)
-                {
-                    Console.WriteLine("FizzBuzz");
-                }
-                else if (i % 5 == 0)
-                {
-                    Console.WriteLine("Buzz");
-                }
-                else if (i % 3 == 0)
-                {
-                    Console.WriteLine("Fizz");   
-                }
-                else
-                {
-                    Console.WriteLine(i);
-                }
-            }
+            var fizzBuzz = new FizzBuzzClass();
+            fizzBuzz.GenerateFizzBuzz();
             Console.ReadLine();
         }
     }
